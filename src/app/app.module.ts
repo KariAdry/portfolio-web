82fa0d14d7b7d820966f622eadbf4fba1c48399b
KariAdri <kalejoerrey@gmail.com>
2022-07-17 14:47:43 -0300

    subo proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { ExperienciaLaboralComponent } from './components/experiencia-laboral/experiencia-laboral.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { SkillsComponent } from './components/skills/skills.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,   
+    ExperienciaLaboralComponent, 
+    AcercaDeComponent, 
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
